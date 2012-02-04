6aaa20819804c7d3614b132a2c057005597fda29
Visore <visore.project@gmail.com>
2012-02-04 15:54:05 +0200

    Old files removed

--- code/essentials/viessentials.h
+++
@@ -1,9 +0,0 @@
-#ifndef VIESSENTIALS_H
-#define VIESSENTIALS_H
-
-#include "vilibrary/vilibrary.h"
-#include "vilogger/vilogger.h"
-#include "viobject/viobject.h"
-
-#endif
-
--- code/system/viaudioconnection/vibassconnection/headers/vibassconnection.h
+++
@@ -1,8 +0,0 @@
-#ifndef VIBASSCONNECTION_H
-#define VIBASSCONNECTION_H
-
-#include "vibassfileinput.h"
-#include "vibassstreaminput.h"
-#include "vibassstreamoutput.h"
-
-#endif
